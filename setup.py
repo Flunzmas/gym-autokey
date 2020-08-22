473fe209750c8fd2b3f5084129bb6c504f79e0ed
Andreas Boltres <andreas.boltres@student.kit.edu>
2020-08-22 10:02:48 +0200

    moved requirements to setup file of gym env

--- setup.py
+++ setup.py
@@ -2,4 +2,14 @@ from setuptools import setup
 
 setup(name='gym-autokey',
       version='0.0.1',
-      install_requires=['gym'])  # And any other dependencies required
+      install_requires=['anytree==2.8.0',
+                        'apted==1.0.3',
+                        'certifi==2020.6.20',
+                        'cloudpickle==1.3.0',
+                        'future==0.18.2',
+                        'gym==0.17.2',
+                        'numpy==1.19.1',
+                        'pyglet==1.5.0',
+                        'scipy==1.5.2',
+                        'six==1.15.0'
+                        ])  # And any other dependencies required
